c7e4775eb161abf8d87c0ecd0fc9c852c027d310
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-07-01 15:16:45 +0900

    [프론트엔드] api/authApi.js에 사용자정보 업데이트 함수 update추가   -  form에 넣어서 api.put요청을 보내도록 설정.   -  axios(인터셉터 설정됨), 비동기함수

--- FrontEnd/src/api/authApi.js
+++ FrontEnd/src/api/authApi.js
@@ -1,19 +1,20 @@
-import api from 'axios';
+import api from '@/api';
 
 const BASE_URL = '/api/member';     //모든 요청은 /api/member를 기준으로 전송
 const headers = { 'Content-Type': 'multipart/form-data' };  //회원가입시 파일업로드(아바타)가 포함되므로, 형식을 변경해줌.
 
 export default {
-  // username 중복 체크, true: 중복(사용불가),  false: 사용 가능
   async checkUsername(username) {
+    // username 중복 체크, true: 중복(사용불가),  false: 사용 가능 - GET방식
     const { data } = await api.get(`${BASE_URL}/checkusername/${username}`);
     console.log('AUTH GET CHECKUSERNAME', data);
     return data;
   },
 
   async create(member) {
+    //사용자 정보 업로드(생성) - POST요청
+
     // 아바타 파일 업로드 – multipart 인코딩 필요(FormData 객체 사용)
-    
     const formData = new FormData();    //formdata객체를 이용해 multipart형식으로 회원정보를 구성함.
     formData.append('username', member.username);
     formData.append('email', member.email);
@@ -28,4 +29,21 @@ export default {
     console.log('AUTH POST: ', data);
     return data;
   },
+
+  async update(member) {  //async = 비동기 함수선언
+    // 사용자 정보 업데이트 - PUT요청
+    const formData = new FormData();
+    formData.append('username', member.username);
+    formData.append('password', member.password);
+    formData.append('email', member.email);
+
+    if (member.avatar) { //avatar는 선택적으로 포함.
+      formData.append('avatar', member.avatar);
+    }
+
+    //axios인스턴스(인터셉터 설정완료) 통해 HTTP PUT 요청을 실행함.
+    const { data } = await api.put(`${BASE_URL}/${member.username}`, formData, headers);
+    console.log('AUTH PUT: ', data);
+    return data;
+  }
 };
